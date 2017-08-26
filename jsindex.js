0b980202aab52f9f447ac13810572e7c0ccdec5b
Iliyan Glavanov <iliqn.glavanov@abv.bg>
2017-08-26 14:09:09 +0300

    Add files via upload
    All the files, I have used, up untill this moment.

---
+++ jsindex.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){    
+    //FANCYBOX
+    //https://github.com/fancyapps/fancyBox
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+
+	$('.dropdown-menu .link').click(function () {
+		$('.dropdown-menu .link').parent().removeClass('active');
+		$(this).parent().addClass('active');
+		
+		var idString = $(this).attr('id');
+		var id = parseInt(idString.substr(6));
+		console.log(id);
+		if (!isNaN(id)) {
+			$('#myCarousel').carousel(id);
+		}
+	});
+});
+
