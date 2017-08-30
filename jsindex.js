45c8536ed923316ff492644a333fc137c229487f
Iliyan Glavanov <iliqn.glavanov@abv.bg>
2017-08-31 00:08:16 +0300

    Carousel-caption-togglebox
    Created a toggle box in the carousel caption. Aswell as a background-color change. Plus a quote in the bottom of the page.

--- jsindex.js
+++ jsindex.js
@@ -1,39 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function()
+{
     //FANCYBOX
     //https://github.com/fancyapps/fancyBox
-    $(".fancybox").fancybox({
+    $(".fancybox").fancybox(
+    {
         openEffect: "none",
         closeEffect: "none"
     });
-
-	$('.dropdown-menu .link').click(function () {
+    //Navbar Link-To Slide START
+	$('.dropdown-menu .link').click(function ()
+  {
 		$('.dropdown-menu .link').parent().removeClass('active');
 		$(this).parent().addClass('active');
 
 		var idString = $(this).attr('id');
 		var id = parseInt(idString.substr(6));
 		console.log(id);
-		if (!isNaN(id)) {
+		if (!isNaN(id))
+    {
 			$('#myCarousel').carousel(id);
 		}
 	});
-  $("#whycarp").click(function(){
-        $(".togglecarp").toggle();
+    // Navbar Link-To Slide END
+
+  // Carousel Caption Toggle START
+      $("#whytilapia").click(function()
+      {
+            $(".toggletilapia").toggle();
+      });
+      $("#whycarp").click(function()
+      {
+            $(".togglecarp").toggle();
+      });
+      $("#whytrout").click(function()
+      {
+            $(".toggletrout").toggle();
+      });
+  // Carousel Caption Toggle END
+  // Video Pause On Scroll START
+    $(window).scroll(function()
+    {
+        var x = $(document).scrollTop();
+        console.log(x);
+        if ($(document).scrollTop() > 350)
+            {
+              $('#videobackground').get(0).pause();
+            }
+            else if ($(document).scrollTop() < 150 )
+            {
+                $('#videobackground').get(0).play();
+              }
     });
-    $(window).scroll(function(){
-    var x = $(document).scrollTop();
-    console.log(x);
+    // Video Pause On Scroll END
 
-    if ($(document).scrollTop() > 350) {
-
-        $('#videobackground').get(0).pause();
-
-        }
-
-        else if ($(document).scrollTop() < 150 ) {
-
-            $('#videobackground').get(0).play();
-
-        }
-    });
 });
